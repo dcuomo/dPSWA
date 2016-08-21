0cd92b5dae7eb991a43f7e59ae1500d4bc27bd44
Dan Cuomo <dacuo@microsoft.com>
2016-08-21 08:53:55 -0400

    Initial Commit
    Creates PowerShell Web Access

---
+++ Examples/SimplePSWA.ps1
@@ -0,0 +1,12 @@
+﻿Configuration PSWA {
+    Import-DscResource -ModuleName dPSWA
+
+    PswaWebApplication PSWA2 {
+        Ensure = 'Absent'
+        WebApplicationName = 'pswa2'
+        WebSiteName = 'PoSHWeb'
+    }
+}
+
+PSWA -OutputPath c:\temp
+Start-DscConfiguration -Path c:\temp -Verbose -Wait -Force
