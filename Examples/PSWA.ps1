86e1613b7e5b0b6508a85f36baa496fa076f83b2
dcuomo <dcuomo@users.noreply.github.com>
2016-08-21 09:17:36 -0400

    Initial Commit

---
+++ Examples/PSWA.ps1
@@ -0,0 +1,33 @@
+﻿Configuration SimplePSWA {
+
+    Import-DSCResource -ModuleName xWebAdministration, PSDesiredStateConfiguration, dPSWA
+    
+    WindowsFeatureSet PowerShellWebAccess {
+        Name   = 'WindowsPowerShellWebAccess','Web-Mgmt-Console'
+        Ensure = 'Present'
+    }
+
+    #Custom WebSite Otherwise, section not needed
+    xWebSite PSWA_Website{
+        Name  = 'PoSHWeb'
+        State = 'Started'
+        Ensure = 'Present'
+        PreloadEnabled = $true
+        PhysicalPath = "$($env:SystemDrive)\inetpub\Tester"
+        BindingInfo     = @(
+            MSFT_xWebBindingInformation {
+                Protocol = "HTTP"
+                Port     = 8080
+            }
+        )
+    }
+
+    PswaWebApplication PSWA2 {
+        Ensure = 'Present'
+        WebApplicationName = 'pswa2'
+        WebSiteName = 'PoSHWeb'
+    }
+}
+
+SimplePSWA -OutputPath c:\temp
+Start-DscConfiguration -Path c:\temp\ -Verbose -Wait -Force
